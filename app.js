93d077577064132b754387b1bca437fe5ed0739c
ken <810768333@qq.com>
2017-05-25 07:20:11 +0800

    1.app.js添加样式。 2.修改标题为“轻松一哈”。

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 //app.js
 App({
+    /*启动函数
+    * */
     onLaunch: function () {
-        //调用API从本地缓存中获取数据
-        var logs = wx.getStorageSync('logs') || []
-        logs.unshift(Date.now())
-        wx.setStorageSync('logs', logs)
+        var logs = wx.getStorageSync('logs') || [];     //调用API从本地缓存中获取数据
+        logs.unshift(Date.now());
+        wx.setStorageSync('logs', logs);    //存入缓存
     },
+    /*获取用户信息
+    *   @param cb获取成功时的回调
+    * */
     getUserInfo: function (cb) {
-        var that = this
+        var that = this;
         if (this.globalData.userInfo) {
             typeof cb == "function" && cb(this.globalData.userInfo)
         } else {
-            //调用登录接口
-            wx.login({
+            wx.login({      //调用登录接口
                 success: function () {
                     wx.getUserInfo({
                         success: function (res) {
-                            that.globalData.userInfo = res.userInfo
-                            typeof cb == "function" && cb(that.globalData.userInfo)
+                            that.globalData.userInfo = res.userInfo;
+                            typeof cb == "function" && cb(that.globalData.userInfo);
                         }
                     })
                 }
             })
         }
     },
+    /*全局变量
+    * */
     globalData: {
         userInfo: null
     }
-})
+});
