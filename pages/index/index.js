bb54c1d1c0186f223849febbc623e823acb9478b
mzk <frontken@sina.com>
2017-05-27 18:06:34 +0800

    1.完成第一板面。

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,26 +1,30 @@
 //index.js
 //获取应用实例
 var app = getApp();
+
 Page({
     data: {
-        motto: 'Hello World',
-        userInfo: {}
-    },
-    //事件处理函数
-    bindViewTap: function () {
-        wx.navigateTo({
-            url: '../logs/logs'
-        })
+        articleList: []
     },
     onLoad: function () {
-        console.log('onLoad');
         var that = this;
-        //调用应用实例的方法获取全局数据
-        app.getUserInfo(function (userInfo) {
-            //更新数据
-            that.setData({
-                userInfo: userInfo
-            })
-        })
+        wx.request({
+            url: 'https://route.showapi.com/341-3',
+            data: {
+                showapi_appid: 28034,
+                showapi_sign: '55f74716416141b1ac2d81797a321538',
+                page: 1,
+                maxResult: 10
+            },
+            success: function (data) {
+                console.log( data.data.showapi_res_body.contentlist );
+                that.setData({
+                    articleList: data.data.showapi_res_body.contentlist
+                });
+            }
+        });
+    },
+    tabClick: function (e) {
+
     }
 });
